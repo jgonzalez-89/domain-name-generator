a0af2975876f98a9355e801a05b0dd243a01bb9e
Jose L <kbcerverjo@gmail.com>
2022-11-12 12:57:43 +0100

    update erros

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".net"];
+let newDomain = [];
+
+
+  for (let i = 0; i < pronoun.length; i++) {
+      for (let i1 = 0; i1 < adj.length; i1++) {
+          for (let i2 = 0; i2 < noun.length; i2++) {
+              for (let i3 = 0; i3 < domain.length; i3++) {
+                 newDomain.push(pronoun[i] + adj[i1] + noun[i2] + domain[i3]);
+                 return '<li>'+pronoun+' '+adj+'</li>'
+              }
+          }
+      }
+  }
+
+document.write(newDomain);
 };
+
+document.querySelector(".prueba").innerHTML = JSON.stringify(newDomain)
