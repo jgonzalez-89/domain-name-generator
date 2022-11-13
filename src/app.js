17020240ae506de9b7723f4285adc223a7fc118a
Jose L <kbcerverjo@gmail.com>
2022-11-13 22:26:12 +0100

    Fix erros

--- src/app.js
+++ src/app.js
@@ -4,22 +4,24 @@ window.onload = function() {
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let domain = [".com", ".net"];
-let newDomain = [];
+let domain = [".com", ".net", ".es", ".eusk", ".lol", ".me"];
 
 
   for (let i = 0; i < pronoun.length; i++) {
       for (let i1 = 0; i1 < adj.length; i1++) {
           for (let i2 = 0; i2 < noun.length; i2++) {
               for (let i3 = 0; i3 < domain.length; i3++) {
-                 newDomain.push(pronoun[i] + adj[i1] + noun[i2] + domain[i3]);
-                 return '<li>'+pronoun+' '+adj+'</li>'
+                let div = document.createElement('div');
+                div.innerText = `
+                    ${pronoun[i]}${adj[i1]}${noun[i2]}${domain[i3]}
+                `
+                document.body.appendChild(div);
+
+
               }
           }
       }
   }
 
-document.write(newDomain);
-};
 
-document.querySelector(".prueba").innerHTML = JSON.stringify(newDomain)
+};
